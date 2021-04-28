e7a10d9cd961b1e04a14cdeb5e6e46a3dec37fec
Ramprit Sahani <ramprit.sahan@mobileprogramming.com>
2021-04-28 13:32:16 +0530

    Adding gitignore

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Domain;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Persistence;
 
 namespace API.Controllers
 {
@@ -12,18 +16,36 @@ namespace API.Controllers
     {
 
         private readonly ILogger<ActivitiesController> _logger;
+        private readonly DataContext _context;
 
-        public ActivitiesController(ILogger<ActivitiesController> logger)
+        public ActivitiesController(ILogger<ActivitiesController> logger, DataContext context)
         {
+            _context = context;
             _logger = logger;
         }
 
         [HttpGet]
-        public IActionResult GetActivities()
+        public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return Ok();
+            var activities = await _context.Activities.ToListAsync();
+            return Ok(activities);
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            var activity = await _context.Activities.FindAsync(id);
+            return Ok(activity);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Activity model)
+        {
+            //TODO: Implement Realistic Implementation
+            await Task.Yield();
+            _logger.LogInformation(model.Title);
+            return Created("", null);
+        }
 
     }
 }
